b1698269b555a61398008f2a1f280fcba588aef1
GD-aish0o <aqsaakqaazi@gmail.com>
2024-08-22 03:09:04 +0500

    major changes and update.

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([
-    { message: "Enter a digit.", type: "number", name: "amOne" },
-    {
-        message: "Please select an operator.",
+// these variables store user input:
+let amOne = await inquirer.prompt({
+    name: "firstAmount",
+    type: "number",
+    message: "Please enter a digit or two ;)",
+});
+// conditions:
+// checks if the provided DT is correct.
+// prints this if not valid:
+if (isNaN(amOne.firstAmount)) {
+    console.error("Provide numbers to be calculated NOT letters and symbols. \n \t \t >_<");
+}
+else {
+    const operators = await inquirer.prompt({
+        name: "Operator",
         type: "list",
-        name: "Operators",
-        choices: ["Add", "Subtract", "Multiply", "Divide", "Calculate Percentage"],
-    },
-    { message: "Enter another digit.", type: "number", name: "amTwo" },
-]);
-// adding switchcase here to detect NaN input and prompt user not to use Nan input in calculator
-// switch (answer) 
-//   case answer.amOne = NaN:
-//   case answer.amTwo = NaN:
-//     console.log("Computer can not calculate Letters or Symbols, Enter Numerical digits only");
-//     break;
-// default:
-if (answer.Operators === "Add") {
-    console.log("Your total amount is: ", +answer.amOne + answer.amTwo);
+        message: "please choose an Operator..!",
+        choices: [
+            "+ Add",
+            "- Subtract",
+            "X Multiply",
+            "/ Divide",
+            "% Calculate Percentage",
+        ],
+    });
+    let amTwo = await inquirer.prompt({
+        name: "SecondAmount",
+        type: "number",
+        message: "Please enter another amount.",
+    });
+    // This if checks if the 2nd DT is valid:
+    if (isNaN(amTwo.SecondAmount)) {
+        console.error("This SumUp-App can only calculate Numbers.!");
+        // if the checkpoints are clear this else program will run:
+    }
+    else {
+        // conditions for different operators chosen,
+        if (operators.Operator == "+ Add") {
+            console.log(Addition(amOne.firstAmount, amTwo.SecondAmount));
+            // calculates
+        }
+        else if (operators.Operator == "- Subtract") {
+            console.log(Subtraction(amOne.firstAmount, amTwo.SecondAmount));
+        }
+        else if (operators.Operator == "X Multiply") {
+            console.log(Multiplication(amOne.firstAmount, amTwo.SecondAmount));
+        }
+        else if (operators.Operator == "/ Divide") {
+            console.log(Division(amOne.firstAmount, amTwo.SecondAmount));
+        }
+        else if (operators.Operator == "% Calculate Percentage") {
+            console.log(persantaij(amOne.firstAmount, amTwo.SecondAmount));
+        }
+    }
+    // Functions
+    function Addition(amOne, amTwo) {
+        return amOne + amTwo;
+        // The code is pretty straightforward, isn't it..?!
+    }
+    function Subtraction(amOne, amTwo) {
+        return amOne - amTwo;
+    }
+    function Multiplication(amOne, amTwo) {
+        return amOne * amTwo;
+    }
+    function Division(amOne, amTwo) {
+        return amOne / amTwo;
+    }
+    function persantaij(amOne, amTwo) {
+        return (amOne / amTwo) * 100;
+    }
 }
-else if (answer.Operators === "Subtract") {
-    console.log("Your total amount is: ", +answer.amOne - answer.amTwo);
-}
-else if (answer.Operators === "Multiply") {
-    console.log("Your total amount is: ", +answer.amOne * answer.amTwo);
-}
-else if (answer.Operators === "Calculate Percentage") {
-    // formla of Percentage
-    console.log("The total percentage of your given amount is:", Math.floor((answer.amOne / answer.amTwo * 100)), "%");
-}
-else if (answer.Operators === "Divide") {
-    console.log("Your total amount is", Math.floor(answer.amOne / answer.amTwo));
-}
-//     break;
-// }
-console.log("\n \n\t\t\t\tThankYou For using SumUp-aish0o.!\n\n");
